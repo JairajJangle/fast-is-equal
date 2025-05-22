3290441b82a1bdef2dff78ba1091cf418e8a8951
Jairaj Jangle <25704330+JairajJangle@users.noreply.github.com>
2025-05-23 01:35:51 +0530

    ci: removed beta build ci and updated ci to temp remove lint job

--- release.config.js
+++ release.config.js
@@ -1,22 +1,12 @@
 module.exports = {
   branches: [
-    'main',
-    {
-      name: 'beta',
-      prerelease: true // Marks this as a prerelease channel
-    }
+    'main'
   ],
   plugins: [
     '@semantic-release/commit-analyzer', // Analyzes commits for version bumping
     '@semantic-release/release-notes-generator', // Generates release notes
     '@semantic-release/changelog', // Generates the changelog
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        tag: 'beta' // Publishes with a 'beta' tag to npm
-      }
-    ],
+    '@semantic-release/npm', // Publishes to npm
     '@semantic-release/github', // Handles GitHub releases
     [
       '@semantic-release/git',
