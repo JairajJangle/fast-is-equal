9c5f38db5b3606da1a1f4186da0b53070a4eabc5
Jairaj Jangle <25704330+JairajJangle@users.noreply.github.com>
2026-07-05 11:10:04 +0530

    chore(release): patch release for readme doc changes

--- release.config.js
+++ release.config.js
@@ -7,7 +7,8 @@ module.exports = {
       '@semantic-release/commit-analyzer', // Analyzes commits for version bumping
       {
         releaseRules: [
-          { type: 'perf', release: 'minor' } // perf commits bump minor (default is patch); other types keep default rules
+          { type: 'perf', release: 'minor' }, // perf commits bump minor (default is patch); other types keep default rules
+          { type: 'docs', scope: 'readme', release: 'patch' } // readme is the npm showcase - republish so npmjs.com picks it up
         ]
       }
     ],
